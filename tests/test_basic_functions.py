5c7e666b0fff73c521d709b65b2db6840f6e9378
mahlidol <137966199+mahlidol@users.noreply.github.com>
2025-10-24 13:42:44 -0500

    Add sys.path configuration for test imports

--- tests/test_basic_functions.py
+++ tests/test_basic_functions.py
@@ -1,4 +1,6 @@
 from rice_ml.basic_functions import add
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 def test_add():
     assert add(2, 3) == 5

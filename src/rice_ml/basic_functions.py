23df761aa993497af99c7643e0365925156bd3db
mahlidol <137966199+mahlidol@users.noreply.github.com>
2025-10-24 13:49:39 -0500

    Clean up basic_functions.py by removing demo function
    Removed unused demo function and import statement.

--- src/rice_ml/basic_functions.py
+++ src/rice_ml/basic_functions.py
@@ -1,11 +1,3 @@
-from rice_ml.distances import euclidian_distance
 
 def add(a, b):
     return a + b
-
-def demo():
-    print(euclidean_distance([1, 2, 3], [4, 5, 6]))
-
-# If you want to run this file directly:
-if __name__ == "__main__":
-    demo()

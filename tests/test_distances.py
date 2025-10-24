7476b3f48efc3f17d73144daf60e14e483d10459
mahlidol <137966199+mahlidol@users.noreply.github.com>
2025-10-24 14:05:34 -0500

    Clean up imports in test_distances.py
    Removed unnecessary sys.path manipulation.

--- tests/test_distances.py
+++ tests/test_distances.py
@@ -1,6 +1,6 @@
+# tests/test_distances.py
+
 from rice_ml.distances import euclidean_distance
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 def test_euclidean_distance():
     assert euclidean_distance([0, 0], [3, 4]) == 5.0

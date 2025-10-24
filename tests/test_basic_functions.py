7b29ea55d28a7db3cf2bbd780b84193b807b45bb
mahlidol <137966199+mahlidol@users.noreply.github.com>
2025-10-24 13:44:26 -0500

    Import add function in test_basic_functions.py

--- tests/test_basic_functions.py
+++ tests/test_basic_functions.py
@@ -1,6 +1,7 @@
-from rice_ml.basic_functions import add
+
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from rice_ml.basic_functions import add
 
 def test_add():
     assert add(2, 3) == 5
